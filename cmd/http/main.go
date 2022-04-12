2e69d38db1efcfbbf78c15204510de072f331f76
William9923 <williamong1400@gmail.com>
2022-04-12 18:58:12 +0700

    feat: modify logger

--- cmd/http/main.go
+++ cmd/http/main.go
@@ -4,16 +4,23 @@ import (
 	"flag"
 	"os"
 
-	log "github.com/sirupsen/logrus"
+	"github.com/sirupsen/logrus"
 )
 
 func main() {
 
+	log := logrus.New()
 	// init logger
-	// get flag when running the service
 	env := flag.String("env", "dev", "environment for running the service")
+	log.SetFormatter(&logrus.TextFormatter{
+		DisableColors:   false,
+		TimestampFormat: "2006-01-02 15:04:05",
+		FullTimestamp:   true,
+	})
 	if *env == "prod" {
-		log.SetFormatter(&log.JSONFormatter{})
+		log.SetFormatter(&logrus.JSONFormatter{
+			TimestampFormat: "2006-01-02 15:04:05",
+		})
 	}
 	log.SetOutput(os.Stdout)
 
