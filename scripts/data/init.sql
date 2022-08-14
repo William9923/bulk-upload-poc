80078ec3cd08b534b7f617d7bf967441633c5d14
William9923 <williamong1400@gmail.com>
2022-08-14 18:56:28 +0700

    feat: add dockerfile + dockercompose (for golang + redis + mysql)

---
+++ scripts/data/init.sql
@@ -0,0 +1,22 @@
+CREATE USER 'admin'@'localhost' IDENTIFIED BY 'admin123';
+SET PASSWORD FOR 'admin'@'localhost' = PASSWORD('admin123');
+
+CREATE DATABASE testing;
+USE testing;
+
+CREATE TABLE employees (
+    emp_no      INT             NOT NULL,  -- UNSIGNED AUTO_INCREMENT??
+    birth_date  DATE            NOT NULL,
+    first_name  VARCHAR(14)     NOT NULL,
+    last_name   VARCHAR(16)     NOT NULL,
+    gender      ENUM ('M','F')  NOT NULL,  -- Enumeration of either 'M' or 'F'  
+    hire_date   DATE            NOT NULL,
+    PRIMARY KEY (emp_no)                   -- Index built automatically on primary-key column
+);
+
+CREATE TABLE departments (
+    dept_no     CHAR(4)         NOT NULL,  -- in the form of 'dxxx'
+    dept_name   VARCHAR(40)     NOT NULL,
+    PRIMARY KEY (dept_no),                 -- Index built automatically
+    UNIQUE  KEY (dept_name)                -- Build INDEX on this unique-value column
+);
